e5407bd6f974c45cee628f41ae34c8e63fb2897c
mbacaa <m.baca.460@studms.ug.edu.pl>
2022-01-16 14:58:30 +0100

    v1.1, grywalne

--- hangman.py
+++ hangman.py
@@ -3,6 +3,7 @@ import random
 import string
 from drawings import drawings
 
+#pobieranie losowego hasła z listy słów, zamiana na wielkie litery, sprawdzenie czy zawiera poprawne znaki (tylko litery)
 def wordValidation(words):
     word = random.choice(words)    
     while word.isalpha() is False:
@@ -16,23 +17,52 @@ def main():
     used_letters = set() 
     alphabet_upper = set(string.ascii_uppercase)
     lives = 6
+    win_condition = secretWord_letters.copy()
 
-    while lives > 0 and len(secretWord_letters) > 0:
-        if len(secretWord_letters) == 0:
-            print("You guessed the word. Congratulations!")
-        else:    
-            player_input = input('Guess a letter: ').upper()
+    while lives > 0 and len(win_condition) > 0:
+        #DO TESTOWANIA
+        print(secretWord)
+        print(secretWord_letters)
+        print(win_condition)
+        #########################
+
+        #"mechanizm" wypisywania podkreśleń/liter w haśle
+        for secretWord_letters in secretWord:
+            if secretWord_letters in used_letters:
+                print(secretWord_letters, " ", end='')
+            else:
+                print("_ ", end='')
+        
+        #główna pętla
+        print('\n')
+        player_input = input('Make a guess: ').upper()
+        
+        if len(player_input) > 1 and player_input != secretWord:
+            lives -=1
+            print("Wrong guess! You've got", lives, "lives left.")
+            continue
+
+        if len(win_condition) != 0 and player_input != secretWord and lives != 0:    
             if player_input in alphabet_upper:
                 used_letters.add(player_input)
-                if player_input in secretWord_letters:
-                    secretWord_letters.remove(player_input)
+                if player_input in win_condition:
+                    win_condition.remove(player_input)
                 else:
                     lives -= 1
                     print("Wrong guess! You've got", lives, "lives left.")
             elif player_input in used_letters:
                 print("Choose the other letter.")
             else:
-                print("It isn't a letter!")
-    print("You lost!")    
+                print("It isn't a letter!") 
 
+        #warunki zakończenia gry: 
+        #wygrana gdy zostanie wpisane całe hasło lub zostaną odgadnięte wszystkie litery
+        if len(win_condition) == 0 or player_input == secretWord:
+            print("You guessed the secret word! Congratulations!")
+            break
+        #przegrana gdy skończą się nam życia
+        elif lives == 0:
+            print("You lost!")
+            break
+        print("Used letters:", used_letters)
 main()
